6d4eb793b376af931544efd6eec30a9c2997eb54
Noel Kujur <lovenoelkujur@gmail.com>
2025-05-17 19:11:56 +0530

    Initial Comit

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const resumeRoutes = require("./routes/resumeRoutes");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+    cors({
+        origin : process.env.CLIENT_URL || "*",
+        methods : ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders : ["Content-Type", "Authorization"],
+    })
+);
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
+
+// Serve Upload folder
+app.use("/uploads",
+    express.static(path.join(__dirname, "uploads"), {
+        setHeaders : (res, path) => {
+            res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+        },
+    })
+);
+// app.use(
+//     "/uploads",
+//     express.static(path.join(__dirname, "uploads"), {
+//         setHeaders : (res, path) => {
+//             // Replace with Hosted frontend URL
+//             const allowedOrigins = ["http://localhost:5173", "https://your-app.onrender.com"];
+//             const origin = res.req.headers.origin;
+            
+//             if(allowedOrigins.includes(origin)){
+//                 res.set("Access-Control-Allow-Origin", origin);
+//             }
+//         },
+//     })
+// );
+
+// Start Server
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+});
